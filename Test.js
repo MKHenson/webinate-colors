7c563d37d71aacb750eb9063557bc4662de3d943
MKHenson <mat@webinate.net>
2015-04-21 18:17:18 +0100

    Moved the source files into the lib folder to keep things neater

--- Test.js
+++
@@ -1,8 +0,0 @@
-var colors = require("./Main");
-colors.log(colors.red("This text should be in red"));
-colors.log(colors.green("This text should be in green"));
-colors.log(colors.yellow("This text should be in yellow"));
-colors.log(colors.yellowBG().blue("This text should be in yellow background with blue text"));
-colors.log(colors.underline("This text should be underlined"));
-colors.log(colors.underline().green("This text should be underlined and in green"));
-colors.log(colors.bold("This text should be bold"));
